6e35adc9771863efcf7227a34b1e6f62192cac18
gustavosalviato <gustavosalviato8@gmail.com>
2024-06-15 15:58:24 -0300

    build: tailwindcss & date-fns

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script>
-  const teste = "";
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<p>
+<p class="text-red-900">
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
