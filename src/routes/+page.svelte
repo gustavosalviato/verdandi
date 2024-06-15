c1f525dcef6f3bfb6e25468c8e2d2c39ffa5c800
gustavosalviato <gustavosalviato8@gmail.com>
2024-06-15 16:09:56 -0300

    feat: add timer base structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,39 @@
 <script>
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p class="text-red-900">
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<div
+  class="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-gray-50"
+>
+  <main class="flex flex-col gap-10 w-full max-w-7xl mx-auto">
+    <h1 class="font-bold text-8xl text-center">25:00</h1>
+
+    <footer class="flex items-center gap-8 mx-auto">
+      <button
+        type="button"
+        class="px-3 py-2 rounded-lg font-medium bg-zinc-900"
+      >
+        Start
+      </button>
+
+      <button
+        type="button"
+        class="px-3 py-2 rounded-lg font-medium bg-zinc-900"
+      >
+        Stop
+      </button>
+
+      <button
+        type="button"
+        class="px-3 py-2 rounded-lg font-medium bg-zinc-900"
+      >
+        Reset
+      </button>
+    </footer>
+
+    <div class="mx-auto flex flex-col gap-6 items-center justify-center">
+      <span>Completed pomodoros:</span>
+      <span class="text-gray-400">Focus Time</span>
+      <span class="text-gray-400">Focus Time</span>
+    </div>
+  </main>
+</div>
