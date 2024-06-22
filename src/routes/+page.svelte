b7be0787d05d48351ab9c59f6135ac3db547ab24
gustavosalviato <gustavosalviato8@gmail.com>
2024-06-22 11:04:04 -0300

    chore: vercel adapter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
   import Countdown from '$lib/Countdown.svelte'
-
-  // $: {
-  //   if (activeCycle) {
-  //     document.title = `Work - ${minutes}:${seconds}`
-  //   }
-  // }
 </script>
 
 <Countdown time={25} shortRestTime={5} longRestTime={15} cyclesNumber={4} />
